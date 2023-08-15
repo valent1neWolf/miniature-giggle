a3e0a3a4e8159379785cf0e77d833815749c213f
valent1neWolf <valentinewolf44v@gmail.com>
2023-08-15 11:20:17 +0200

    md5 changes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
-require('dotenv').config();
+require('dotenv').config();   //ennek a kód elején kell lennie
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');   //nem ajánlott a használata
 
 const app = express();
 app.use(express.static("public"));
@@ -22,7 +23,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{ secret: process.env.SECRET, encryptedFields:["password"]});
+
 
 const User = new mongoose.model("User",userSchema)
 
@@ -41,7 +42,7 @@ app.get("/register",function (req,res) {
 app.post("/register",function (req,res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save()
   .then(function(){
@@ -55,7 +56,7 @@ app.post("/register",function (req,res) {
 
 app.post("/login",function (req,res) {
 const username = req.body.username;
-const password = req.body.password;
+const password = md5(req.body.password);
 User.findOne({email: username})
 .then(function (foundUser) {
   if(foundUser.password === password){
